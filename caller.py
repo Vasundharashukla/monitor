7fec7b2cf0af30756fb05cd5c9afd9f823330388
Vasundhara Shukla <32017029+Vasundharashukla@users.noreply.github.com>
2020-06-12 23:02:01 +0530

    added caller file to call routes

---
+++ caller.py
@@ -0,0 +1,51 @@
+import requests
+from pprint import pprint
+
+url = "http://monitoring-env.eba-kmh3ybxg.ap-south-1.elasticbeanstalk.com"
+
+def create():
+    resp = requests.post(f"{url}/create-user", json={"username": "vasu", "password": "vasu", "email": "17ucc065@lnmiit.ac.in"})
+    print(resp.json())
+
+def add():
+    resp = requests.post(f"{url}/add-instances", json={"username": "vasu", "password": "vasu",
+                                                                      "InstanceId":'i-05bdf36c7e6fb9961'})
+    print(resp.json())
+
+def get():
+    resp = requests.get(f"{url}/get-instances", json={"username": "vasu", "password": "vasu"})
+    print(resp.json())
+
+def halt():
+    resp = requests.post(f"{url}/halt", json={"username": "vasu", "password": "vasu", "InstanceId": ['i-08afd5a0960ad677f']})
+    print(resp.json())
+
+def metrics():
+    resp = requests.get(f"{url}/api/fork/metrics", json={"username": "vasu", "password": "vasu", "attributes": ["CPUUtilization", "NetworkIn"]}, verify=False)
+    pprint(resp.json())
+
+def meta():
+    resp = requests.get(f"{url}/api/fork/metadata", json={"username": "vasu", "password": "vasu"})
+    pprint(resp.json())
+
+def setp():
+    resp = requests.post(f"{url}/set/interval", json={"username": "vasu", "password": "vasu", "interval": 1})
+    pprint(resp.json())
+
+def getdata():
+    resp = requests.get(f"{url}/get-data", json={"username": "vasu", "password": "vasu"})
+    pprint(resp.json())
+
+def thresh():
+    resp = requests.post(f"{url}/check-threshold", json={"username": "vasu", "password": "vasu"})
+    pprint(resp.json())
+
+#create()
+#add()
+#metrics()
+#meta()
+#halt()
+#setp()
+#getdata()
+#get()
+#thresh()
